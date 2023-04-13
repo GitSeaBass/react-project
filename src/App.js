6741a66f7f3d80602b2258356cc6466615918c92
Sebastian Garcia <sag44402@uga.edu>
2023-04-13 14:11:32 -0400

    Added Static Json Arrays

--- src/App.js
+++ src/App.js
@@ -4,10 +4,34 @@ import NavBar from './components/NavBar';
 
 const DUMMY_ITEMS = [
 {
-
+  id: 'i1', // id of specific post
+  title: '', // title of post
+  description: '', // description by user
+  image: '', // url of image
+  date: '', // date posted
+  user: '', // user who posted
+  comments: [ // all comments on post
+    {user: "", comment: ""}, // user and comment
+    {user: "", comment: ""}, // user and comment
+  ],
+  likes: [ // likes on post
+    '', // users who liked
+  ],
 },
 {
-
+  id: 'i2', 
+  title: 'Title',
+  description: 'This is a description',
+  image: '',
+  date: new Date(2023, 1, 1),
+  user: 'user01',
+  comments: [
+    {user: "user02", comment: "Nice!"},
+    {user: "user06", comment: "Miss u"},
+  ],
+  likes: [
+    "user02", "user03", "mydad"
+  ],
 }
 ];
 
@@ -22,14 +46,12 @@ function App() {
   const[isLoggedIn, setIsLoggedIn] = useState(false);
   const onLoggedIn = () => {
     setIsLoggedIn(() => {
-      return true;
+      return !isLoggedIn;
     });
   }
 
   return (
-    <>
     <NavBar loggedIn={isLoggedIn} onLoggedIn={onLoggedIn}/>
-    </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ function NavBar(props) {
     return (
       <div>
         <img src="" alt="Logo"/>
-        {props.loggedIn ? <button>Creat Post</button>:<button onClick={props.onLoggedIn}>Login</button>}
+        {props.loggedIn ? <button onClick={props.onLoggedIn}>Create Post</button>:<button onClick={props.onLoggedIn}>Login</button>}
         {props.loggedIn ? <></>:<button>Signup</button>}
       </div>
     );
