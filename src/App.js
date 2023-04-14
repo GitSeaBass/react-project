df54ed57d647bc5e33497289a0e62b90b8873e3b
Sebastian Garcia <sag44402@uga.edu>
2023-04-13 20:18:41 -0400

    Added css for NavBar

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ const App = () => {
     <Router>
       <div>
         <Routes>
-          <Route exact path='/' element={<NavBar loggedIn={isLoggedIn}/>} />
+          <Route exact path='/' element={<NavBar loggedIn={isLoggedIn} onLoggedIn={onLoggedIn}/>} />
           <Route path='/login' element={<LoginPage loggedIn={isLoggedIn} onLoggedIn={onLoggedIn}/>} />
         </Routes>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,20 @@ import {Link} from 'react-router-dom';
 function NavBar(props) {
     return (
       <div className='NavBar'>
-        <img src="" alt="Logo" className='logo'/>
-        {props.loggedIn ? <button className='button'>Create Post</button>:
-          <Link to='/login'>
-            <button className='button'> Sign In</button>
-          </Link>
-        } 
-        {props.loggedIn ? <></>:<button>Signup</button>}
+        <div className='brand'>
+          <img src="" alt="Logo" className='logo'/>
+          <h3>Title</h3>
+        </div>
+
+        <div className='buttons'>
+          {props.loggedIn ? <button className='button'>Create Post</button>:
+            <Link to='/login'>
+              <button className='button'> Sign In</button>
+            </Link>
+          } 
+          {props.loggedIn ? <button className='button' onClick={props.onLoggedIn}>Logout</button>:
+            <button className='button'>Signup</button>}
+        </div>
       </div>
     );
 }
